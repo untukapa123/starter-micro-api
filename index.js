48b30f14790eb2dc9888504a150065815a61d98a
untukapa123 <129593576+untukapa123@users.noreply.github.com>
2023-10-17 15:27:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-addEventListener('fetch', event => {
-    event.respondWith(proxy(event));
-});
+const http = require('http');
+const https = require('https');
+const { URL } = require('url');
 
-async function proxy(event) {
-    let url = new URL(event.request.url);
-    let pathname = url.pathname;
-    let sub_pathname_arr = pathname.split("/");
-    const domain = sub_pathname_arr[1];
-    sub_pathname_arr.splice(0, 2);
-    pathname = '/' + sub_pathname_arr.join('/');
-    url.pathname = pathname;
-    url.hostname = domain;
-    event.request.headers.Host = domain;
-    return fetch(new Request(url, event.request));
-}
+http.createServer((req, res) => {
+    if (req.url.startsWith('/')) {
+        const targetUrl = `https:${req.url}`; // Konstruksi URL target dari permintaan
+        console.log(`Proxying request to: ${targetUrl}`);
+
+        const options = new URL(targetUrl);
+        const requestOptions = {
+            hostname: options.hostname,
+            path: options.pathname + options.search,
+            method: req.method,
+            headers: req.headers
+        };
+
+        const proxyReq = https.request(requestOptions, (proxyRes) => {
+            res.writeHead(proxyRes.statusCode, proxyRes.headers);
+            proxyRes.pipe(res, { end: true });
+        });
+
+        req.pipe(proxyReq, { end: true });
+
+        proxyReq.on('error', (error) => {
+            console.error('Error:', error);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+}).listen(process.env.PORT || 3000);
