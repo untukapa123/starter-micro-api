829a47a7b7b520ef2ee9a11f8bbc71232d1d5caf
untukapa123 <129593576+untukapa123@users.noreply.github.com>
2023-10-17 14:55:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+addEventListener('fetch', event => {
+    event.respondWith(proxy(event));
+});
+
+async function proxy(event) {
+    let url = new URL(event.request.url);
+    let pathname = url.pathname;
+    let sub_pathname_arr = pathname.split("/");
+    const domain = sub_pathname_arr[1];
+    sub_pathname_arr.splice(0, 2);
+    pathname = '/' + sub_pathname_arr.join('/');
+    url.pathname = pathname;
+    url.hostname = domain;
+    event.request.headers.Host = domain;
+    return fetch(new Request(url, event.request));
+}
