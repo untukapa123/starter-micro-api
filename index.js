28e2cd2391b2841ca882fb339bd3b921d0f98f6a
untukapa123 <129593576+untukapa123@users.noreply.github.com>
2023-10-17 15:18:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-const http = require('http');
-const httpProxy = require('http-proxy');
+addEventListener('fetch', event => {
+    event.respondWith(proxy(event));
+});
 
-// Konfigurasi target URL
-const target = 'https://d3j4fjrwclc3o8.cloudfront.net/CH1/indexCH1.m3u8';
-
-const proxy = httpProxy.createProxyServer({});
-
-http.createServer((req, res) => {
-    // Set CORS headers
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-    if (req.method === 'OPTIONS') {
-        // Handle preflight requests
-        res.writeHead(200);
-        res.end();
-    } else {
-        // Proxy the request to the target URL
-        proxy.web(req, res, { target });
-    }
-}).listen(process.env.PORT || 3000);
+async function proxy(event) {
+    let url = new URL(event.request.url);
+    let pathname = url.pathname;
+    let sub_pathname_arr = pathname.split("/");
+    const domain = sub_pathname_arr[1];
+    sub_pathname_arr.splice(0, 2);
+    pathname = '/' + sub_pathname_arr.join('/');
+    url.pathname = pathname;
+    url.hostname = domain;
+    event.request.headers.Host = domain;
+    return fetch(new Request(url, event.request));
+}
